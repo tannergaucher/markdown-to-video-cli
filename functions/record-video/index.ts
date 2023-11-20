addfd92913d0b1ce27af32fe19692125925594ff
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-11-20 09:55:29 -0500

    Add puppeteer-screen-recorder dependency

---
+++ functions/record-video/index.ts
@@ -0,0 +1,4 @@
+/*
+Use puppeteer to record a video of the dom. The audio plays, firing the video text animations. 
+Then stitch the video file with no audio together with the audio file in ffmpeg.
+*/
